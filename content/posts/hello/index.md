69765c5740eb018171d2d2b9c8d76be1f4bbee09
Casey Link <unnamedrambler@gmail.com>
2025-03-25 15:17:00 +0100

    And now markdown

---
+++ content/posts/hello/index.md
@@ -0,0 +1,14 @@
+{
+  :title "hello world blog post"
+  :tags #{:one :two}
+}
+
+# Hello World
+
+
+This is a markdown post.
+
+
+```clojure
+(println "Hello World")
+```
--- src/site/content.clj
+++ src/site/content.clj
@@ -1,20 +1,67 @@
 (ns site.content
   (:require
-   [clj-org.org :as org]
-   [clojure.java.io :as io]))
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [nextjournal.markdown :as md]
+   [nextjournal.markdown.transform :as md.transform]))
+
+(defn- s2sr [s]
+  (-> s (java.io.StringReader.) (java.io.BufferedReader.)))
+
+(defn parse-edn-metadata-headers
+  [lines-seq]
+  (if (re-matches #"\{.*" (or (first lines-seq) ""))
+    ;; take sequences until you hit an empty line
+    (let [meta-lines (take-while (comp not (partial re-matches #"\s*"))
+                                 lines-seq)]
+      [(->> meta-lines
+            ;; join together and parse
+            (str/join "\n")
+            edn/read-string)
+       ;; count the trailing empty line
+       (inc (count meta-lines))])
+    [nil 0]))
+
+(parse-edn-metadata-headers (line-seq (s2sr "{
+  :title \"wut\"
+  :tags #{:one :two}
+}
+
+# Hello World
+
+
+This is a markdown post with EDN frontmatter
+")))
+;; => [{:title "wut", :tags #{:one :two}} 5]
+
+(parse-edn-metadata-headers (line-seq (s2sr "# Hello World")))
+;; => [nil 0]
+
+(parse-edn-metadata-headers (line-seq (s2sr "")))
+;; => [nil 0]
+
+(defn md->hiccup [string]
+  (let [s                     (-> string s2sr line-seq)
+        [metadata meta-lines] (parse-edn-metadata-headers s)]
+    [metadata
+     (->> (drop meta-lines s)
+          (str/join "\n")
+          md/parse
+          md.transform/->hiccup)]))
 
 (defn parse [path]
-  (let [file                            (io/file (str "content/" path "/index.org"))
-        {:keys [title headers content]} (-> file
-                                            slurp
-                                            org/parse-org)]
-    {:title   title
-     :headers headers
-     :content content}))
+  (let [file               (io/file (str "content/" path "/index.md"))
+        [metadata content] (-> file slurp md->hiccup)]
+    {:metadata metadata
+     :title    (:title metadata)
+     :content  content}))
 
 (defn content [page-fn dir req]
   (page-fn
    (parse (str dir "/" (-> req :path-params :slug)))))
 
 (comment
-  (parse "posts/hello"))
+  (parse "posts/hello")
+  ;; rcf
+  )
