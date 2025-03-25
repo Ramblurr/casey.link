9ea541feb92e3397cb16c03b465e2693aab523f0
Casey Link <unnamedrambler@gmail.com>
2025-03-25 09:44:27 +0100

    running from cli

--- src/site/server.clj
+++ src/site/server.clj
@@ -19,13 +19,17 @@
     {:handler (fn [req]
                 {:status 200 :body "Hello, World!"})
      :server  #::ds{:start  (fn [{config ::ds/config}]
-                              (server/run-server (:handler config)
-                                                 {:port                 (:port config)
-                                                  :legacy-return-value? false}))
+                              (let [instance (server/run-server (:handler config)
+                                                                {:port                 (:port config)
+                                                                 :ip                   (:host config)
+                                                                 :legacy-return-value? false})]
+                                (println (format "started listening at %s:%s" (:host config) (:port config)))
+                                instance))
                     :stop   (fn [{server ::ds/instance}]
                               (when server
                                 (server/server-stop! server)))
                     :config {:handler (ds/local-ref [:handler])
+                             :host    (ds/ref [:env :host])
                              :port    (ds/ref [:env :port])}}}}})
 
 (defn env-config [& [profile]]
