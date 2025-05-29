75ae9d9e89ecd1b111b4797b41769582c52e6560
Casey Link <unnamedrambler@gmail.com>
2025-05-29 08:30:19 +0200

    Responsive sidenotes in gutters

--- src/site/ui/article_layout.clj
+++ src/site/ui/article_layout.clj
@@ -16,21 +16,14 @@
 
 (defn article-layout
   [{:keys [article children previous-pathname]}]
-  (container/container {:class "mt-16 lg:mt-32"}
-                       [:div {:class "xl:relative"}
-                        [:div {:class "mx-auto max-w-2xl"}
-                         (when previous-pathname
-                           [:button {:type          "button"
-                                     :aria-label    "Go back to articles"
-                                     :class         "cursor-pointer group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white ring-1 shadow-md shadow-zinc-800/5 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-                                     :data-on-click "window.history.go(-1)"}
-                            (arrow-left-icon "h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400")])
-                         [:article
-                          [:header {:class "flex flex-col"}
-                           [:h1 {:class "mt-6 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100"}
-                            (:title article)]
-                           [:time {:datetime (:date article)
-                                   :class    "order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"}
-                            [:span {:class "h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"}]
-                            [:span {:class "ml-3"} (format-date (:date article))]]]
-                          (prose/prose {:class "mt-8"} children)]]]))
+  [:div {:class "sm:px-8 mt-16 lg:mt-32"}
+   [:section {:id "article-container" :class ""}
+    [:article {:class "max-w-2xl lg:max-w-5xl"}
+     [:header {:class "flex flex-col"}
+      [:h1 {:class "mt-6 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100"}
+       (:title article)]
+      [:time {:datetime (:date article)
+              :class    "order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"}
+       [:span {:class "h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"}]
+       [:span {:class "ml-3"} (format-date (:date article))]]]
+     (prose/prose {:class "mt-8"} children)]]])
