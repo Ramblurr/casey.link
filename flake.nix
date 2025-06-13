6cd1f0519a7958915e58da8544e344fffeb650e1
Casey Link <unnamedrambler@gmail.com>
2025-06-13 13:01:34 +0200

    Add nix build tooling

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "casey.link";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
+    flake-utils.url = "github:numtide/flake-utils";
+    clj-nix.url = "github:jlesquembre/clj-nix";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      clj-nix,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        javaVersion = 24;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = clj-nix.lib.mkCljApp {
+            inherit pkgs;
+            modules = [
+              {
+                projectSrc = ./.;
+                name = "me.casey.link/www";
+                main-ns = "site.server";
+                java-opts = [
+                  "-Duser.timezone=UTC"
+                  "-XX:+UseZGC"
+                  "--enable-native-access=ALL-UNNAMED"
+                ];
+                jdk = pkgs."jdk${toString javaVersion}";
+                #customJdk.enable = true;
+                buildCommand = ''
+                  export PATH=${pkgs.tailwindcss_4}/bin:$PATH
+                  clj -T:build uber
+                '';
+              }
+            ];
+          };
+
+        };
+
+        overlays.default =
+          final: prev:
+          let
+            jdk = prev."jdk${toString javaVersion}";
+          in
+          {
+            clojure = prev.clojure.override { inherit jdk; };
+          };
+
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.babashka
+              pkgs.caddy
+              pkgs.clj-kondo
+              pkgs.clojure
+              pkgs.clojure-lsp
+              pkgs.flyctl
+              pkgs.tailwindcss_4
+              pkgs.vips
+              pkgs.zsh
+            ];
+          };
+        };
+      }
+    );
+}
