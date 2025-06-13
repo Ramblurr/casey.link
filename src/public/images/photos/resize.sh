36c13ebc946ec5452ccdba86a167dd0ee28d06e0
Casey Link <unnamedrambler@gmail.com>
2025-06-13 11:23:47 +0200

    Update photos

--- src/public/images/photos/resize.sh
+++ src/public/images/photos/resize.sh
@@ -2,16 +2,25 @@
 sizes=(176 288 472 672)
 
 for size in "${sizes[@]}"; do
-  height=$((size * 10 / 9))
-
-for img in orig/*.{jpg,png,webp}; do
+  for img in home/*.{jpg,png,webp}; do
     [ -f "$img" ] || continue
     base=$(basename "${img%.*}")
     vips thumbnail "$img" \
       "${base}-${size}w.webp[Q=85,strip]" \
       ${size} \
-      --height=${height} \
-      --size=force \
+      --export-profile=srgb \
+      --intent=perceptual
+  done
+done
+
+sizes=(300 600 900 1236)
+for size in "${sizes[@]}"; do
+  for img in about/*.{jpg,png,webp}; do
+    [ -f "$img" ] || continue
+    base=$(basename "${img%.*}")
+    vips thumbnail "$img" \
+      "${base}-${size}w.webp[Q=85,strip]" \
+      ${size} \
       --export-profile=srgb \
       --intent=perceptual
   done
